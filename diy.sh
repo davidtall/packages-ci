7771d2b12852e8c0db66ff02e51ca11ce8d14f17
davidtall <269700927@qq.com>
2025-06-09 16:23:25 +0800

    Update shell

---
+++ diy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git clone https://github.com/QiuSimons/luci-app-daed tmp
+cd tmp && mv {daed,luci-app-daed} ../
+cd .. && rm -rf tmp
+git clone https://github.com/davidtall/OpenWRT-CI tmp
+cd tmp && mv package/* ../
+
