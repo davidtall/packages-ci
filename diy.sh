7db9931e53e11037cc416073f771f787d3af7b6d
david <davidtall@qq.com>
2025-08-11 02:52:27 +0800

    增加dae

--- diy.sh
+++ diy.sh
@@ -3,6 +3,6 @@
 git clone https://github.com/QiuSimons/luci-app-daed tmp
 cd tmp && mv {daed,luci-app-daed} ../
 cd .. && rm -rf tmp
-git clone https://github.com/davidtall/OpenWRT-CI tmp
-cd tmp && mv package/* ../
-cd .. && rm -rf tmp
+#git clone https://github.com/davidtall/OpenWRT-CI tmp
+#cd tmp && mv package/* ../
+#cd .. && rm -rf tmp
