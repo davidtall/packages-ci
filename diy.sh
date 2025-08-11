5cbf021cd4e8781ca53b191880dc5bf68ea662d6
david <davidtall@qq.com>
2025-08-11 11:46:50 +0800

    替换Makefile

--- diy.sh
+++ diy.sh
@@ -3,6 +3,8 @@
 git clone https://github.com/QiuSimons/luci-app-daed tmp
 cd tmp && mv {daed,luci-app-daed} ../
 cd .. && rm -rf tmp
+sed -i 's/PKG_MIRROR_HASH:=skip/PKG_MIRROR_HASH:=/g' daed/Makefile
+
 #git clone https://github.com/davidtall/OpenWRT-CI tmp
 #cd tmp && mv package/* ../
 #cd .. && rm -rf tmp
