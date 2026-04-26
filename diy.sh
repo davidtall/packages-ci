a8a993c82dd9fb187db05a1ecaf4d53f2408ce28
david <269700927@qq.com>
2026-04-26 22:27:22 +0800

    修改

--- diy.sh
+++ diy.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 git clone https://github.com/QiuSimons/luci-app-daed tmp
-cd tmp && mv {daed,luci-app-daed} ../
-cd .. && rm -rf tmp
-sed -i 's/PKG_MIRROR_HASH:=skip/PKG_MIRROR_HASH:=/g' daed/Makefile
+pushd tmp
+  mv {daed,luci-app-daed} ../
+popd
 
-git clone https://github.com/davidtall/OpenWRT-CI tmp
-cd tmp && mv package/* ../
-cd .. && rm -rf tmp
+sed -i 's/PKG_MIRROR_HASH:=skip/PKG_MIRROR_HASH:=/g' daed/Makefile
+sed -i 's/pnpm install ; \\/pnpm install --no-frozen-lockfile ; \\/g' daed/Makefile
+sed -i 's|/run/i\\  procd_set_param|/procd_set_param command/i \\\tprocd_set_param|g' luci-app-daed/root/etc/init.d/luci_daed
+
+rm -rf tmp && git clone https://github.com/davidtall/OpenWRT-CI tmp
+pushd tmp
+  mv package/* ../
+popd
+rm -rf tmp
