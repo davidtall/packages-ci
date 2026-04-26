d6e5ec69dea911f94b863ad6d6f2ac5032c99173
david <269700927@qq.com>
2026-04-26 22:03:19 +0800

    修改

--- diy.sh
+++ diy.sh
@@ -5,6 +5,6 @@ cd tmp && mv {daed,luci-app-daed} ../
 cd .. && rm -rf tmp
 sed -i 's/PKG_MIRROR_HASH:=skip/PKG_MIRROR_HASH:=/g' daed/Makefile
 
-#git clone https://github.com/davidtall/OpenWRT-CI tmp
-#cd tmp && mv package/* ../
-#cd .. && rm -rf tmp
+git clone https://github.com/davidtall/OpenWRT-CI tmp
+cd tmp && mv package/* ../
+cd .. && rm -rf tmp
